3b06744c0bf7be49addfc1219282e50a0ce69d25
andrefei <andreas.eikeland@hotmail.com>
2018-11-14 14:48:29 -0800

    Add files for testing C integration

--- sensor-pi/index.js
+++ sensor-pi/index.js
@@ -1,16 +1,30 @@
+//Send sensor data to React server
+//by using the socket.io engine
+
+'use strict';
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
+let addon = require('./build/Release/integer');
+
+//Create Socket.io server
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const app = express();
 app.use(index);
 
-const server = http.createServer(app);
+//Buffer variables
+let nrOne = new Buffer("1");
+let nrTwo = new Buffer("40000");
 
-const io = socketIo(server); // < Interesting!
+//	Call the C++ function with our numbers, and store the result in a new
+//	variable
+let sum = addon.sum(nrOne, nrTwo);
 
+//Sends data to client on connection
 io.on("connection", socket => {
   console.log("New client connected"), setInterval(
     () => emitData(socket),
@@ -20,7 +34,7 @@ io.on("connection", socket => {
 });
 const emitData = async socket => {
   try {
-    socket.emit("FromAPI", "test-data");
+    socket.emit("FromAPI", sum);
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
---
+++ sensor-pi/main.cpp
@@ -0,0 +1,51 @@
+#include <nan.h>
+#include <iostream>
+
+using namespace std;
+using namespace Nan;
+using namespace v8;
+
+//
+//	The function that we are going to call from NodeJS
+//
+NAN_METHOD(Sum)
+{
+	//
+	//	1.	Save the buffers that I passed from NodeJS in to local variables
+	//
+	unsigned int nrOne = info[0]->Uint32Value();
+	unsigned int nrDwo = info[1]->Uint32Value();
+
+	//
+	//	2.	Sum the two numbers together
+	//
+	uint32_t sum = nrOne + nrDwo;
+
+	//
+	//	->	Send the buffer back to NodeJS with the result of our calculation.
+	//
+	info
+	.GetReturnValue()
+	.Set(
+		Nan::New<Uint32>(sum));
+}
+
+//
+//	The constructor
+//
+NAN_MODULE_INIT(Init)
+{
+	//
+	//	Expose the method or methods to NodeJS
+	//
+	Nan::Set(
+		target,
+		New<String>("sum").ToLocalChecked(),
+		GetFunction(New<FunctionTemplate>(Sum)).ToLocalChecked()
+	);
+}
+
+//
+//	Load the constructor
+//
+NODE_MODULE(basic_nan, Init)
